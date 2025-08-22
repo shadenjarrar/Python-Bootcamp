127ffb5af39448fed399397b300e4d7832b91ed6
shadenjarrar <shadin.jarrar@gmail.com>
2025-08-22 19:07:43 +0300

    Add files via upload

---
+++ shaden_jarrar3.py
@@ -0,0 +1,11 @@
+u="MyUserName"
+p="mypassword123"
+fu=u.lower()
+l=len(p)
+print("Username:",fu)
+print("Password length:",l)
+po=l>=8
+ad=fu=="admin"
+print("Password ok?",po)
+print("Is admin?",ad)
+print("Welcome,",fu)
